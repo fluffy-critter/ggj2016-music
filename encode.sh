c43750a826d901d7a7347dac2b174621520b86b4
fluffy <fluffy@beesbuzz.biz>
2016-01-29 23:12:27 -0800

    better microsite format

--- encode.sh
+++ encode.sh
@@ -16,6 +16,12 @@ cat << EOF
 <head>
 <title>Global Game Jam 2016 music</title>
 
+<style>
+table {border-collapse: collapse; }
+table td { border: solid black 1px; }
+table th { border: solid black 3px; }
+</style>
+
 <!-- jQuery -->
 <script src="js/jquery-1.11.0.min.js"></script>
 
@@ -37,21 +43,27 @@ cat << EOF
 </head>
 
 <body>
-    <p>Here is the music I've made for this year's Global Game Jam (2016). Please see <a href="https://github.com/plaidfluff/ggj2016-music">my GitHub repo</a> for more information.</p>
+    <p>Here is the music I've made for this year's Global Game Jam (2016), most recent first. Please see <a href="https://github.com/plaidfluff/ggj2016-music">my GitHub repo</a> for more information.</p>
 <table>
+<tr><th>Folder</th><th>Name</th><th>mp3</th><th>ogg</th><th>flac</th><th>Updated</th></tr>
 EOF
 
-lastdir=''
-find . -type f -name '*.mp3' | sort | while read mp3 ; do
+find . -type f -name '*.mp3' | while read mp3 ; do
+    printf '%d %s\n' $(stat -f %m "$mp3") "$mp3"
+done | sort -nr | while read mtime mp3 ; do
+    echo mtime=$mtime mp3=$mp3 1>&2
     dir="$(dirname "$mp3" | cut -f2- -d/)"
-    if [ "$dir" != "$lastdir" ]; then
-        printf '<tr><th colspan=5>%s</th></tr>' "$dir"
-        lastdir="$dir"
-    fi
     basename="$(basename "$mp3" .mp3)"
     pathpart="$(dirname "$mp3")/$basename"
     fmtime="$(stat -t "%Y-%m-%d %H:%M" -f %Sm "$mp3")"
-    printf '<tr><td>%s</td><td>%s</td><td><a href="%s">mp3</a></td><td><a href="%s.ogg">ogg</a></td><td><a href="%s.flac">flac</a></td></tr>' "$fmtime" "$basename" "$mp3" "$pathpart" "$pathpart"
+    printf '<tr>
+    <td>%s</td>
+    <td>%s</td>
+    <td><a href="%s">mp3</a></td>
+    <td><a href="%s.ogg">ogg</a></td>
+    <td><a href="%s.flac">flac</a></td>
+    <td>%s</td>
+    </tr>' "$dir" "$basename" "$mp3" "$pathpart" "$pathpart" "$fmtime"
 done
 printf '</table>'
 
